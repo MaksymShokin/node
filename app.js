d6360feefc11582eb10e0ca6f2822eeddd561ebd
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-03 19:38:55 +0300

    'my-first-response'

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const http = require('http');
 
 const rqListener = (req, res) => {
   console.log(req);
-  // stop function
-  process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first response</title></head>');
+  res.write('<body><h1>Hello from node.js</h1></body>');
+  res.write('<html>');
+  res.end();
 }
 
 const server = http.createServer(rqListener)
